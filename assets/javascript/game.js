654924a36497bea03fa2b6263cb87a7e22208dff
Spinglez <42229372+Spinglez@users.noreply.github.com>
2018-10-11 11:35:24 -0700

    all functions are in place, html is built but needs to be styled, working on IF statements for the game to finish and restart TODO: finish if statements figure out what I need to compare to to ge the game to recognize the score starting at 0 and being the same as the players number.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,9 @@
-var goalNum = "";
+var goalNum = null;
 var skullNum = [null,null,null,null];
 var wins = 0;
 var loss = 0;
-var guessNum = "";
+var guessNum = 0;
+var gameOn = true;
 
 function goalGen() {
   goalNum = Math.floor(Math.random() * 100) + 19;
@@ -20,21 +21,54 @@ reset = function(){
   var goalNum = "";
   var skullNum = [null,null,null,null];
   var guessNum = "";
-  start();
+  start()
 }
 
 function increment(x){
   x++;
+  return x;
 }
 
 function start(){
   goalGen();
   skullGen();
-  $('#skull1').val(skullNum[0]);
-  $('#skull2').val(skullNum[1]);
-  $('#skull3').val(skullNum[2]);
-  $('#skull4').val(skullNum[3]);
+  $('#random').html('<h2>'+goalNum+'</h2>');
+  $('#wins').html('Total Wins: ' + wins);
+  $('#loss').html('Total Losses: ' + loss);
+  $('#score').html('<h2>'+guessNum+'</h2>');
+}
+
+// trying to build my if statements
+if (guessNum == goalNum) {
+  increment(wins);
+  $('#announce').html('<p>You WIN!</p>');
+  reset();
+}
+else {
+  increment(loss);
+  $('#announce').html('<p>You LOSE!</p>');
+  reset();
 }
 
 start();
 console.log(goalNum,skullNum);
+
+// on click events are below
+  $('#skull1').on('click', function(){
+    guessNum = guessNum + skullNum[0];
+    $('#score').html('<h2>'+guessNum+'</h2>');
+  })
+
+  $('#skull2').on('click', function(){
+    guessNum = guessNum + skullNum[1];
+    $('#score').html('<h2>'+guessNum+'</h2>');
+  })
+
+  $('#skull3').on('click', function(){
+    guessNum = guessNum + skullNum[2];
+    $('#score').html('<h2>'+guessNum+'</h2>');
+  })
+  $('#skull4').on('click', function(){
+    guessNum = guessNum + skullNum[3];
+    $('#score').html('<h2>'+guessNum+'</h2>');
+  })
