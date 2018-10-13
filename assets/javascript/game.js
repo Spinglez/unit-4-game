b9a97aeff351f118ae64f63ade1fb5fddd64a4c6
Spinglez <42229372+Spinglez@users.noreply.github.com>
2018-10-13 16:34:54 -0700

    Added sounds and worked up some styling, committing now so I don't lose changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,11 @@ var wins = 0;
 var loss = 0;
 var guessNum = 0;
 var gameOn = true;
+var winSound = new Audio('assets/sounds/veryImpressive.mp3');
+var loseSound = new Audio('assets/sounds/skeletorLaughs.mp3');
+var startSound = new Audio('assets/sounds/powerBall.mp3')
 
+// resets variables after game is completed thanks scott for pointing out my syntax errors
 reset = function(){
   goalNum = 0;
   skullNum = [null,null,null,null];
@@ -12,18 +16,21 @@ reset = function(){
   start();
 }
 
+// Generates a random goal number
 function goalGen() {
   goalNum = Math.floor(Math.random() * 100) + 19;
   return goalNum;
   console.log(goalNum);
 }
 
+// generates an array containing each skulls secret number
 function skullGen() {
     for (var i = 0; i < skullNum.length; i++) {
       skullNum[i] = Math.floor(Math.random() * 11) + 1;
   }
 }
 
+// displays player information on the page
 function setup(){
   $('#random').html('<h2>'+goalNum+'</h2>');
   $('#wins').html('Total Wins: ' + wins);
@@ -31,27 +38,32 @@ function setup(){
   $('#score').html('<h2>'+guessNum+'</h2>');
 }
 
+// begins the game and also starts it again after it is finished
 function start() {
   goalGen();
   skullGen();
   setup();
 }
 
+// adds the player guess to the guessNum variable and continues to add it while
+// looking for victory or loss conditions also increments the score which is then displayed by setup.
+
 function increaseNum(num) {
   guessNum += num;
   if (guessNum == goalNum) {
     wins++;
     $('#announce').html('<p>You WIN!</p>');
+    winSound.play();
     reset();
   }
   else if (guessNum > goalNum) {
     loss++;
     $('#announce').html('<p>You LOSE!</p>');
+    loseSound.play();
     reset();
   }
   else {
   }
-console.log(goalNum,skullNum);
 }
 
 // on click events are below
@@ -76,5 +88,6 @@ console.log(goalNum,skullNum);
   })
 
 start();
-
-console.log(guessNum);
+setTimeout(function(){
+  startSound.play();
+},1000);
