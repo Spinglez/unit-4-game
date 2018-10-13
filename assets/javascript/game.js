5baf749a32e06e9b5acccbd6278c02e4238c17ae
Spinglez <42229372+Spinglez@users.noreply.github.com>
2018-10-13 16:01:53 -0700

    game is fully functional, removed the increment function as it wasn't returning the wins correctly. added a setup function to clean up the other functions. TODO: Finish styling the page.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,16 @@
-var goalNum = null;
+var goalNum = 0;
 var skullNum = [null,null,null,null];
 var wins = 0;
 var loss = 0;
 var guessNum = 0;
 var gameOn = true;
+
+reset = function(){
+  goalNum = 0;
+  skullNum = [null,null,null,null];
+  guessNum = 0;
+  start();
+}
 
 function goalGen() {
   goalNum = Math.floor(Math.random() * 100) + 19;
@@ -17,58 +24,57 @@ function skullGen() {
   }
 }
 
-reset = function(){
-  var goalNum = "";
-  var skullNum = [null,null,null,null];
-  var guessNum = "";
-  start()
-}
-
-function increment(x){
-  x++;
-  return x;
-}
-
-function start(){
-  goalGen();
-  skullGen();
+function setup(){
   $('#random').html('<h2>'+goalNum+'</h2>');
   $('#wins').html('Total Wins: ' + wins);
   $('#loss').html('Total Losses: ' + loss);
   $('#score').html('<h2>'+guessNum+'</h2>');
 }
 
-// trying to build my if statements
-if (guessNum == goalNum) {
-  increment(wins);
-  $('#announce').html('<p>You WIN!</p>');
-  reset();
-}
-else {
-  increment(loss);
-  $('#announce').html('<p>You LOSE!</p>');
-  reset();
+function start() {
+  goalGen();
+  skullGen();
+  setup();
 }
 
-start();
+function increaseNum(num) {
+  guessNum += num;
+  if (guessNum == goalNum) {
+    wins++;
+    $('#announce').html('<p>You WIN!</p>');
+    reset();
+  }
+  else if (guessNum > goalNum) {
+    loss++;
+    $('#announce').html('<p>You LOSE!</p>');
+    reset();
+  }
+  else {
+  }
 console.log(goalNum,skullNum);
+}
 
 // on click events are below
   $('#skull1').on('click', function(){
-    guessNum = guessNum + skullNum[0];
+    increaseNum(skullNum[0]);
     $('#score').html('<h2>'+guessNum+'</h2>');
   })
 
   $('#skull2').on('click', function(){
-    guessNum = guessNum + skullNum[1];
+    increaseNum(skullNum[1]);
     $('#score').html('<h2>'+guessNum+'</h2>');
   })
 
   $('#skull3').on('click', function(){
-    guessNum = guessNum + skullNum[2];
+    increaseNum(skullNum[2]);
     $('#score').html('<h2>'+guessNum+'</h2>');
   })
+
   $('#skull4').on('click', function(){
-    guessNum = guessNum + skullNum[3];
+    increaseNum(skullNum[3]);
     $('#score').html('<h2>'+guessNum+'</h2>');
   })
+
+start();
+
+console.log(guessNum);
